4cd35872c4fb9b73f713c20aba6460add6b4d329
Lmcmax <lmcmax99@gmail.com>
2025-03-25 00:26:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// testing .....
+let x = "i am a Hacker ."
+const y = 1
+
+function lmc(a,b,c=12) {
+    console.log(x , y)
+    console.log("try to kncock me ...🚀")
+    console.log(String(a)+" ==> " , b+c)
+    console.log("\t This web sight is 🎨 updateing ...")
+    return 0;
+}
+
+console.log(lmc("\t(^_^ )",1 , 2))
+
+// tested <<<<<<<<<<<<<
+
+function what() {
+    var a = prompt("Enter password : ???")
+
+    if (String(a) == "ok") {
+        window.open("test.html")
+        console.log("ok 🎨")
+    } else {
+        console.log("Nothing ... (^_^ ) 😊")
+    }
+    return 0 ;
+}
+
+//😃 😊 😎 🤖 😍 🤔 🤯 🤓 😈 🤡 🧐 😱 😂 🤭 😤 🥶 🥳 😵 😇 🤩
+
+function alerting() {
+    alert("This websight is want to be updated ...")
+    console.log("\t\n\t==> 404")
+}
+
+// for(let i = 100 ; i < 10 ; i++ , sum = 0 ){
+//     sum = sum + i
+//     console.log(sum)
+// }
+
